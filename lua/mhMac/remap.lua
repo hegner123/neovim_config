8de2ecd5ba63a14b337ba000cca1c0a17a11b16d
Michael Hegner <hegner123@gmail.com>
2024-03-31 08:50:34 -0400

    updated remaps

--- lua/mhMac/remap.lua
+++ lua/mhMac/remap.lua
@@ -4,6 +4,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("i", "<C-Q>", "<Esc>")
 vim.keymap.set("n", "Q", "<Nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
